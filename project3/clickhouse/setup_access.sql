fa7654fcf62f9b36c02aaa5b862a028ead390b23
kyarish <kyarish@kse.org.ua>
2025-11-30 22:31:10 +0200

    added access

--- project3/clickhouse/setup_access.sql
+++ project3/clickhouse/setup_access.sql
@@ -4,7 +4,7 @@ CREATE ROLE IF NOT EXISTS analyst_limited;
 
 -- 2. Create users
 CREATE USER IF NOT EXISTS analyst_full_user IDENTIFIED BY 'full123';
-CREATE USER IF NOT EXISTS analyst_limited_user IDENTIFIED BY 'limited123';
+CREATE USER IF NOT EXISTS analyst_limited_user IDENTIFIED BY 'lim123';
 
 -- 3. Assign roles to users
 GRANT analyst_full   TO analyst_full_user;
@@ -15,15 +15,19 @@ GRANT SELECT ON youtube_dbt.dim_channel            TO analyst_full;
 GRANT SELECT ON youtube_dbt.dim_video              TO analyst_full;
 GRANT SELECT ON youtube_dbt.fact_channel_metrics   TO analyst_full;
 GRANT SELECT ON youtube_dbt.fact_video_performance TO analyst_full;
+GRANT SELECT ON youtube_dbt.fact_channel_metrics   TO analyst_full;
+GRANT SELECT ON youtube_dbt.fact_video_performance TO analyst_full;
 
-GRANT SELECT ON youtube_dbt.top_10_channels_full   TO analyst_full;
-GRANT SELECT ON youtube_dbt.top_10_videos_full     TO analyst_full;
+GRANT SELECT ON youtube_gold.snapshot_dim_channel  TO analyst_full;
+GRANT SELECT ON youtube_gold.snapshot_dim_video     TO analyst_full;
 
 -- 5. Permissions for LIMITED analyst
 GRANT SELECT ON youtube_dbt.dim_channel            TO analyst_limited;
 GRANT SELECT ON youtube_dbt.dim_video              TO analyst_limited;
 GRANT SELECT ON youtube_dbt.fact_channel_metrics   TO analyst_limited;
 GRANT SELECT ON youtube_dbt.fact_video_performance TO analyst_limited;
+GRANT SELECT ON youtube_gold.snapshot_dim_channel  TO analyst_limited;
+GRANT SELECT ON youtube_gold.snapshot_dim_video     TO analyst_limited;
 
 GRANT SELECT ON youtube_dbt.top_10_channels_limited TO analyst_limited;
 GRANT SELECT ON youtube_dbt.top_10_videos_limited   TO analyst_limited;
